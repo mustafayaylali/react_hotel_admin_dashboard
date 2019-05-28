15bd1d6cc038c9aa492ec19dce9aa2bbb2e7893f
mustafayaylali <mustafayaylali_@hotmail.com>
2019-05-28 22:05:41 +0300

    rooms

--- src/views/Rooms/Rooms.js
+++ src/views/Rooms/Rooms.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { CardFooter, Col, Row, Progress } from 'reactstrap';
+import React, { Component, Text } from 'react';
+import { CardFooter, Col, Row, Progress, Button } from 'reactstrap';
 import Widget04 from '../Widgets/Widget04';
 
 import roomsData from './RoomsData'
@@ -10,7 +10,7 @@ function RoomRow(props) {
 
   const getBadge = (status) => {
     return status === 'Active' ? 'success' :
-      status === 'Inactive' ? 'secondary' :
+      status === 'Inactive' ? 'primary ' :
         status === 'Pending' ? 'warning' :
           status === 'Banned' ? 'danger' :
             'primary'
@@ -25,40 +25,46 @@ function RoomRow(props) {
 
 class Users extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {normalButton:'true',
+                selectButton:'none'};
+  }
+
   render() {
 
-    const roomList = roomsData.filter((room) => room.id < 10)
+    const roomList = roomsData.filter((room) => room.id < 10) //gösterilecek veri adedi
 
     return (
       <div className="animated fadeIn">
         <CardFooter>
-                <Row className="text-center">
-                  <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">CHART KOY YUVARLAK</div>
-                    <strong>12 Oda (40%)</strong>
-                    <Progress className="progress-xs mt-2" color="success" value="40" />
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0 d-md-down-none">
-                    <div className="text-muted">Temizleniyor</div>
-                    <strong>2 Oda (10%)</strong>
-                    <Progress className="progress-xs mt-2" color="info" value="20" />
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">Rezervasyon</div>
-                    <strong>5 Oda (30%)</strong>
-                    <Progress className="progress-xs mt-2" color="danger" value="80" />
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">Dolu</div>
-                    <strong>4 Oda (20%)</strong>
-                    <Progress className="progress-xs mt-2" color="warning" value="60" />
-                  </Col>
-                </Row>
-              </CardFooter>
+          <Row className="text-center">
+            <Col sm={12} md className="mb-sm-2 mb-0">
+              <Button size="lg" block outline color="success" style={{display:this.state.normalButton}}>Boş : 12</Button>
+              <Button size="lg" className="btn-brand mr-1 mb-1" color="success" style={{display:this.state.selectButton}}>
+                <i className="fa fa-thumbs-up"></i>
+                <span style={{ fontSize: 15 }}>Boş : 12 </span>
+              </Button>
+            </Col>
+            <Col sm={12} md className="mb-sm-2 mb-0">
+              <Button size="lg" className="btn-brand mr-1 mb-1" color="warning" style={{display:this.state.visible}}>
+                <i class="fa fa-hourglass-half"></i>
+                <span style={{ fontSize: 15 }}>Temizleniyor : 4 </span></Button>
+            </Col>
+            <Col sm={12} md className="mb-sm-2 mb-0">
+              <Button size="lg" className="btn-brand mr-1 mb-1" color="danger"><i class="fa fa-lock"></i>
+                <span style={{ fontSize: 15 }}>Dolu : 6 </span></Button>
+            </Col>
+            <Col sm={12} md className="mb-sm-2 mb-0">
+              <Button size="lg" className="btn-brand mr-1 mb-1" color="primary"><i class="fa fa-phone"></i>
+                <span style={{ fontSize: 15 }}>Rezerve : 2  </span></Button>
+            </Col>
+          </Row>
+        </CardFooter>
 
-         {//icon-lock  icon-like icon-phone icon-emotsmile
-         } 
-         <br></br>
+        {//icon-lock  icon-like icon-phone icon-emotsmile
+        }
+        <br></br>
         <Row>
           {roomList.map((room, index) =>
             <RoomRow key={index} room={room} />
